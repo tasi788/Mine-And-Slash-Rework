e4e747132fa6e2ab34e0c8208d35b3f14aea41d7
Robert Skalko <treborx555@gmail.com>
2024-11-07 19:20:17 +0100

    5.9.8

--- src/main/java/com/robertx22/mine_and_slash/database/data/league/LeagueStructurePieces.java
+++ src/main/java/com/robertx22/mine_and_slash/database/data/league/LeagueStructurePieces.java
@@ -20,8 +20,8 @@ public class LeagueStructurePieces implements IWeighted {
         try {
             ChunkPos start = MapData.getStartChunk(pos.getMiddleBlockPosition(50));
             ChunkPos relative = new ChunkPos(pos.x - start.x, pos.z - start.z);
-            int x = Math.abs(relative.x);
-            int z = Math.abs(relative.z);
+            int x = relative.x;
+            int z = relative.z;
 
             if (isWithinBounds(x, z)) {
                 return SlashRef.id("league/" + folder + "/" + x + "_" + z);
